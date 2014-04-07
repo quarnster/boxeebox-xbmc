10634fb4932c78c121ea7452f3f7ef6f6ca45890
Q <q@Q-VirtualBox.(none)>
2014-04-07 12:46:29 +0100

    Add EGL,GLES2 & KHR headers

---
+++ tools/boxeebox/libs/avahi/EGL/eglplatform.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#define EGLAPI
+#define EGLAPIENTRY
+typedef int NativeDisplayType;
+typedef void *NativeWindowType;
+typedef void *NativePixmapType;
+typedef NativeDisplayType EGLNativeDisplayType;
+typedef NativePixmapType  EGLNativePixmapType;
+typedef NativeWindowType  EGLNativeWindowType;
+typedef int EGLint;
